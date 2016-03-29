ba922a0b4377a243bd891b8d522ed6fec7c3a335
Trafalgar-Square <schneller.demian@gmail.com>
2016-03-29 11:08:27 +0200

    Revert "Kang BoardCommonConfig.mk from CyanogenMod"
    This reverts commit 0481ea78083d4557bc7dbdeb7faaaa1bbec13e03.

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-# This variable is set first, so it can be overridden
+# This variable is set first, so it can be overriddenlp
 # by BoardConfigVendor.mk
 USE_CAMERA_STUB := true
 BOARD_USES_GENERIC_AUDIO := false
@@ -30,8 +30,18 @@ TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_VARIANT := cortex-a9
 ARCH_ARM_HAVE_NEON := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+
+# Board already specifies -mcpu, but it won't hurt to add mtune, too
+BOARD_GLOBAL_CFLAGS += -mtune=cortex-a9
+BOARD_GLOBAL_CPPFLAGS += -mtune=cortex-a9
+
+# Specify L1/L2 caches used for Exynos 4412
+BOARD_GLOBAL_CFLAGS += --param l1-cache-line-size=32 --param l1-cache-size=32 --param l2-cache-size=1024
+BOARD_GLOBAL_CPPFLAGS += --param l1-cache-line-size=32 --param l1-cache-size=32 --param l2-cache-size=1024
+
+# Hint the compiler that we're using quad-core CPU
+BOARD_GLOBAL_CFLAGS += -mvectorize-with-neon-quad
+BOARD_GLOBAL_CPPFLAGS += -mvectorize-with-neon-quad
 
 EXYNOS4X12_ENHANCEMENTS := true
 EXYNOS4_ENHANCEMENTS := true
@@ -67,6 +77,7 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12381585408
 BOARD_FLASH_BLOCK_SIZE := 4096
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USES_BLOCK_BASED_OTA := false
 
 # Hardware tunables
 BOARD_HARDWARE_CLASS := hardware/samsung/cmhw
@@ -76,27 +87,29 @@ BOARD_EGL_CFG := device/samsung/smdk4412-common/configs/egl.cfg
 BOARD_EGL_NEEDS_HANDLE_VALUE := true
 USE_OPENGL_RENDERER := true
 BOARD_USES_SKIAHWJPEG := true
-COMMON_GLOBAL_CFLAGS += -DSEC_HWJPEG_G2D -DFORCE_SCREENSHOT_CPU_PATH -DWORKAROUND_BUG_10194508 -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+COMMON_GLOBAL_CFLAGS += -DSEC_HWJPEG_G2D -DWORKAROUND_BUG_10194508 -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
+BOARD_USE_MALI_ALIGNMENT := true
+
+# HWC
+BOARD_USES_PROPRIETARY_HWC := true
 
 # FIMG Acceleration
 BOARD_USES_FIMGAPI := true
 BOARD_USES_SKIA_FIMGAPI := true
+BOARD_USES_SKIA_FIMGAPI_BOOSTUP := true
 
 # Enable WEBGL in WebKit
 ENABLE_WEBGL := true
 
-# HWComposer
-BOARD_USES_HWCOMPOSER := true
-BOARD_USE_SYSFS_VSYNC_NOTIFICATION := true
+# HDMI
+BOARD_USES_HDMI := true
+BOARD_USES_SAMSUNG_HDMI := true
+BOARD_HDMI_DDC_CH := DDC_CH_I2C_5
 
 # Camera
 BOARD_CAMERA_HAVE_ISO := true
 COMMON_GLOBAL_CFLAGS += -DHAVE_ISO
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
 COMMON_GLOBAL_CFLAGS += -DSAMSUNG_DVFS
-BOARD_USES_PROPRIETARY_LIBFIMC := true
-BOARD_CAMERA_MSG_MGMT := true
 
 # OMX
 BOARD_USE_SAMSUNG_COLORFORMAT := true
@@ -106,17 +119,12 @@ BOARD_USE_METADATABUFFERTYPE := true
 BOARD_USES_MFC_FPS := true
 BOARD_USE_S3D_SUPPORT := true
 BOARD_USE_CSC_FIMC := false
+BOARD_CANT_REALLOCATE_OMX_BUFFERS := true
 
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
-# Logging
-TARGET_USES_LOGD := false
-
 BOARD_USES_LEGACY_MMAP := true
-
-# RIL
-BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
 
 # Wifi
 BOARD_WLAN_DEVICE                := bcmdhd
@@ -147,8 +155,6 @@ BOARD_BLUEDROID_VENDOR_CONF := device/samsung/smdk4412-common/bluetooth/vnd_smdk
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
 
 # Recovery
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/smdk4412-common/recovery/recovery_keys.c
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/smdk4412-common/recovery/graphics.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
 BOARD_USES_MMCUTILS := true
@@ -167,12 +173,10 @@ RED_LED_PATH := /sys/class/leds/led_r/brightness
 GREEN_LED_PATH := /sys/class/leds/led_g/brightness
 BLUE_LED_PATH := /sys/class/leds/led_b/brightness
 BACKLIGHT_PATH := /sys/class/backlight/panel/brightness
+BOARD_CHARGER_SHOW_PERCENTAGE := true
 
 # Override healthd HAL
 BOARD_HAL_STATIC_LIBRARIES := libhealthd.exynos4
-
-# LPM Battery Percentage
-BOARD_CHARGER_SHOW_PERCENTAGE := true
 
 # inherit from the proprietary version
 -include vendor/samsung/smdk4412-common/BoardConfigVendor.mk
